7e0b7c2299a0c252a5f2fbdd502aff563892fd79
Simon Nicolussi <sinic@sinic.name>
2022-01-22 21:32:44 +0100

    Rename function and move it below the commands

--- grammaticus.el
+++ grammaticus.el
@@ -48,18 +48,6 @@
       (add-hook 'post-command-hook #'grammaticus-lookup t t)
     (remove-hook 'post-command-hook #'grammaticus-lookup t)))
 
-(defun grammaticus--check ()
-  "Check word at point and underline it if it's unknown."
-  (let ((result (grammaticus--get grammaticus--db (thing-at-point 'word t))))
-    (when-let* ((at (bounds-of-thing-at-point 'word)))
-      (remove-overlays (car at) (cdr at) 'grammaticus-overlay t)
-      (when (seq-every-p #'car result)
-        (let ((overlay (make-overlay (car at) (cdr at) nil t))
-              (color (if result "DarkOrange" "Red1")))  ; like in Flyspell
-          (overlay-put overlay 'face `(:underline (:style wave :color ,color)))
-          (overlay-put overlay 'grammaticus-overlay t)))
-      (grammaticus--show result))))
-
 (defun grammaticus-lookup (&optional word)
   "Look up grammatical information to a WORD and display it."
   (interactive (list (read-string "Look up word: " (current-word))))
@@ -91,6 +79,18 @@
                             (push (point) (gethash key (cdr grammaticus--db))))
                           (forward-line))))
   (message "Database now has %d words" (hash-table-size (cdr grammaticus--db))))
+
+(defun grammaticus--highlight ()
+  "Check word at point and underline it if it's unknown."
+  (let ((result (grammaticus--get grammaticus--db (thing-at-point 'word t))))
+    (when-let* ((at (bounds-of-thing-at-point 'word)))
+      (remove-overlays (car at) (cdr at) 'grammaticus-overlay t)
+      (when (seq-every-p #'car result)
+        (let ((overlay (make-overlay (car at) (cdr at) nil t))
+              (color (if result "DarkOrange" "Red1")))  ; like in Flyspell
+          (overlay-put overlay 'face `(:underline (:style wave :color ,color)))
+          (overlay-put overlay 'grammaticus-overlay t)))
+      (grammaticus--show result))))
 
 (defun grammaticus--get (db word)
   "Return list of pairs with information pertaining to WORD."
